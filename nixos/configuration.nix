942409473fe373853f1bcf6b1731f07d75b19fd8
gulp1n <abeldenouden@hotmail.com>
2024-08-26 22:19:45 +0200

    maybe this'll work

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -102,14 +102,18 @@
   systemd.user.services.GoRadio = {
     name = "GoRadio";
     enable = true;
-    path = pkgs.buildGo122Module {
-      name = "GoRadio";
-      src = pkgs.fetchgit {
-        url = "https://github.com/AVTOLZ/GoRadio.git";
-        rev = "aed51d62a45a01d47d263c4db8350b4b7b4c5b88";
-        hash = "sha256-UC/F9WbVyYuAhOLxMgnIyzd+1aVBrFLQG8xPm7udL8Q=";
-      };
-      vendorHash = "sha256-LEoWUO/TyLONd8m0D1hki+WvmLO09JyESuH+eHRWoSc=";
+    serviceConfig = {
+      ExecStart = pkgs.buildGo122Module {
+
+        name = "GoRadio";
+        src = pkgs.fetchgit {
+          url = "https://github.com/AVTOLZ/GoRadio.git";
+          rev = "aed51d62a45a01d47d263c4db8350b4b7b4c5b88";
+          hash = "sha256-UC/F9WbVyYuAhOLxMgnIyzd+1aVBrFLQG8xPm7udL8Q=";
+        };
+        vendorHash = "sha256-LEoWUO/TyLONd8m0D1hki+WvmLO09JyESuH+eHRWoSc=";
+
+      }; 
     };
   };
 
